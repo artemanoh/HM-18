42866a4b9f4ff61517c8ccac8a735eb7ec15eb4d
artem1214 <61391756+artemanoh@users.noreply.github.com>
2025-02-13 17:17:08 +0200

    make hm-18

--- index.js
+++ index.js
@@ -63,3 +63,8 @@ const increment = function () {
     valueDisplay.textContent = currentCount;
 };
 
+const decrementButton = document.querySelector('button[data-action="decrement"]');
+const incrementButton = document.querySelector('button[data-action="increment"]');
+
+decrementButton.addEventListener("click", decrement);
+incrementButton.addEventListener("click", increment);
